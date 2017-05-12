f977729dd2cf8053a8e15fa24d57ab551c2836c1
HKK13 <Kaan_K193@hotmail.com>
2017-05-13 00:59:33 +0300

    Add temporary reducer

--- src/redux/Reducers/BoardReducer.jsx
+++ src/redux/Reducers/BoardReducer.jsx
@@ -12,39 +12,62 @@ function findItemIndex(list, id) {
     });
 }
 
+/**
+ * Crappy Reducer
+ */
 const reducer = (state = {}, action) => {
     let swapState = Object.assign({}, state),
         listIndex = null,
-        itemIndex = null;
+        itemIndex = null,
+        items = null,
+        list = null,
+        lists = null;
 
     switch(action.type) {
         case 'addItem':
+            //Wow such immutable very design
             listIndex = findListIndex(swapState.lists, action.list);
-
-            swapState.lists[listIndex].push(action.item);
+            lists = swapState.lists.slice();
+            list = Object.assign({}, lists[listIndex]);
+            items = list.items.slice();
+            items.push({id: action.item, description: ''});
+            list.items = items;
+            lists[listIndex] = list;
+            swapState.lists = lists;
             return swapState;
 
         case 'deleteItem':
+            //Wow such immutable very design
             listIndex = findListIndex(swapState.lists, action.list);
-            itemIndex = findItemIndex(swapState.lists[listIndex], action.item);
+            itemIndex = findItemIndex(swapState.lists[listIndex].items, action.item);
+            lists = swapState.lists.slice();
+            list = Object.assign({}, lists[listIndex]);
+            items = list.items.slice();
+            items.splice(itemIndex, 1);
+            list.items = items;
+            lists[listIndex] = list;
+            swapState.lists = lists;
+            return swapState;
 
-            swapState.lists[listIndex].splice(itemIndex, 1);
             return swapState;
 
         case 'addList':
-            if(!swapState.lists)
-                return {lists: [{id: action.id, title: '', children: []}]};
-
-            swapState.lists.push({id: action.id, title: '', children: []});
+            lists = swapState.lists.slice();
+            lists.push({id: action.id, title: '', items: []});
+            swapState.lists = lists;
             return swapState;
 
         case 'deleteList':
             listIndex = findListIndex(swapState.lists, action.id);
-            swapState.lists.splice(listIndex, 1);
+            lists = swapState.lists.slice();
+            lists.splice(listIndex, 1);
+            swapState.lists = lists;
+            return swapState;
+
             return swapState;
 
         default:
-            return state;
+            return swapState;
     }
 };
 
